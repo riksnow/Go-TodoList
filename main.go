9ff5298255283533508a5f27874ae35e114cc27f
riksnow <riksnow1000@gmail.com>
2026-01-06 00:45:26 +0300

    Creates TODO apis that uses a slice as a DB

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
+}
+
+func main() {
+	fmt.Println("Hello World")
+	app := fiber.New()
+	err := godotenv.Load(".env")
+	if err != nil{
+		log.Fatal("Error loading .env file")
+	}
+
+	PORT := os.Getenv("PORT")
+
+	todos := []Todo{}
+
+	//Get TODOS
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	// Get TODO
+	app.Get("/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+		for _, todo := range todos {
+			if id == fmt.Sprint(todo.ID) {
+				return c.Status(202).JSON(todo)
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+	
+
+	// Create a TODO
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // {id:0, completed:false, body:""}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	// Update a TODO
+	app.Put("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos{
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
+	// Delete a TODO
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if id == fmt.Sprint(todo.ID) {
+				todos = append(todos[:i], todos[i+1:]...)
+				return c.Status(202).JSON(todos)
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "TODO not found"})
+	})
+
+	log.Fatal(app.Listen(":" + PORT))
+}
